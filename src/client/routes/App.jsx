77ca4da73339e17f805185e972ee5c49afee6fed
Michael Diolosa <michael.diolosa@gmail.com>
2016-08-16 13:25:11 -0400

    Decorators are now supported

--- src/client/routes/App.jsx
+++ src/client/routes/App.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-export default class App extends React.Component {
-  doAsync() {
-    return Promise.resolve();
-  }
-
-  render() {
-    return <div>Hello, App!</div>;
-  }
+export default function App() {
+  return <div>Hello, App!</div>;
 }
