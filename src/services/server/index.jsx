dc9ce3e53f2779160a02279bfc4988105beeb406
Michael Diolosa <michael.diolosa@gmail.com>
2016-08-18 20:14:22 -0400

    Begin work on migrating to forever to run our application

--- src/services/server/index.jsx
+++ src/services/server/index.jsx
@@ -1,40 +1,11 @@
-import Server from './Server';
-import routes from '../../client/routes';
+require('babel-register')();
+const Server = require('./Server');
+const routes = require('../../client/routes');
 
-let server = null;
+const server = new Server({ routes });
 
-/**
- * Starts the isomorphic web server.
- *
- * @return {Promise} - Returns the promise associated with starting the server.
- */
-export function start() {
-  if (server) { throw new Error('The server is already started.'); }
-  server = new Server({ routes });
-
-  return server.start().then(s => {
-    /* eslint-disable no-console */
-    console.log(`Server started in ${process.env.NODE_ENV} mode on port ${server.options.port}.`);
-    /* eslint-enable no-console */
-
-    return s;
-  });
-}
-
-/**
- * Stops the isomorphic web server.
- *
- * @return {Promise} - Returns the promise associated with stopping the server.
- */
-export function stop() {
-  if (!server) { throw new Error('The server has not started.'); }
-
-  return server.stop().then(s => {
-    server = null;
-    /* eslint-disable no-console */
-    console.log('Server stopped.');
-    /* eslint-enable no-console */
-
-    return s;
-  });
-}
+server.start().then(() => {
+  /* eslint-disable no-console */
+  console.log(`Server started in ${process.env.NODE_ENV} mode on port ${server.options.port}.`);
+  /* eslint-enable no-console */
+});
